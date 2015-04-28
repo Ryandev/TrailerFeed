0d43b7efa0cbc646c4d1c966c1fef73056c2ecf4
Ryan Powell <ryangarethpowell@gmail.com>
2015-04-28 19:28:32 +0100

    Updated internet connection test url

--- trailerfeed.py
+++ trailerfeed.py
@@ -33,7 +33,7 @@ def isInternetConnectionAvailable():
     try:
         '''Google IP'''
         import urllib2
-        response=urllib2.urlopen('http://74.125.228.100',timeout=5)
+        response=urllib2.urlopen('http://www.google.com',timeout=5)
         return True
     except urllib2.URLError as err:
         return False
